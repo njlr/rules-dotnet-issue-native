395b61689dd3ffea69c9b9a3abd4ac29f4e1e7bf
njlr <njlr@users.noreply.github.com>
2022-08-28 21:33:44 +0100

    * Applies fix

--- sync-paket-deps.sh
+++ sync-paket-deps.sh
@@ -6,6 +6,5 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
   cd "$SCRIPT_DIR"
   bazel run @rules_dotnet//tools/paket2bazel:paket2bazel.exe -- \
     --dependencies-file $(pwd)/paket.dependencies \
-    --output-folder $(pwd)/deps \
-    --config $(pwd)/paket2bazel.config.json
+    --output-folder $(pwd)/deps
 )
